61066484c8c042058561863e252ceb650f070f00
LegendaryLegend <tyconner@hotmail.com>
2014-05-30 13:58:43 -0400

    Create shared.lua

---
+++ gamemode/shared.lua
@@ -0,0 +1,10 @@
+GM.Name = "Fear"
+GM.Author = "Legend"
+GM.Email = "tyconner@hotmail.com"
+GM.Website = "N/A"
+
+team.SetUp( 0, "Victims", Color(0, 0, 255) )
+
+function GM:Initialize()
+	self.BaseClass.Initialize( self )
+end
